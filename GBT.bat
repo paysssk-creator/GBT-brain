494b836d9f317f6fed9fc963a3621d4f294b41c3
自由的风 <payzelle@zohomail.com>
2026-06-13 19:17:45 +0700

    evolve: GBT全局启动命令 - 一行启动所有脑功能

---
+++ GBT.bat
@@ -0,0 +1,9 @@
+@echo off
+chcp 65001 >nul 2>nul
+cd /d %USERPROFILE%\.gbt\vector_db 2>nul
+if not exist launch.py (echo GBT not found && exit /b 1)
+set PYTHONIOENCODING=utf-8
+(where python 2>nul || where python3 2>nul) >%temp%\gbt_py.txt
+set /p PY=<%temp%\gbt_py.txt
+del %temp%\gbt_py.txt 2>nul
+%PY% launch.py %*
