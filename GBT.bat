78c68298f73c46310646670757619dade92ac947
paysssk-creator <hello@gbtxiaotudou.com>
2026-06-17 00:11:46 +0700

    evolve: smart Chrome detection in GBT.bat and startup-landing.bat

--- GBT.bat
+++ GBT.bat
@@ -1,5 +1,12 @@
 @echo off
 chcp 65001 >nul 2>nul
 title GBT小土豆全能开发者
-start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --new-window --window-size=1400,900 "%USERPROFILE%\.gbt\launcher.html"
+:: Smart Chrome detection - try multiple install paths
+set "CHROME="
+if exist "C:\Program Files\Google\Chrome\Application\chrome.exe" set "CHROME=C:\Program Files\Google\Chrome\Application\chrome.exe"
+if exist "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" set "CHROME=C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+if exist "%LOCALAPPDATA%\Google\Chrome\Application\chrome.exe" set "CHROME=%LOCALAPPDATA%\Google\Chrome\Application\chrome.exe"
+if "%CHROME%"=="" if exist "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" set "CHROME=C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+if "%CHROME%"=="" if exist "C:\Program Files\Microsoft\Edge\Application\msedge.exe" set "CHROME=C:\Program Files\Microsoft\Edge\Application\msedge.exe"
+if "%CHROME%"=="" (start "" "%USERPROFILE%\.gbt\launcher.html") else (start "" "%CHROME%" --new-window --window-size=1400,900 "%USERPROFILE%\.gbt\launcher.html")
 exit /b 0
--- startup-landing.bat
+++ startup-landing.bat
@@ -1,4 +1,10 @@
 @echo off
 chcp 65001 >nul 2>nul
 :: GBT小土豆全能开发者 - 开机自启
-"C:\Program Files\Google\Chrome\Application\chrome.exe" --new-window "%USERPROFILE%\.gbt\launcher.html"
+set "CHROME="
+if exist "C:\Program Files\Google\Chrome\Application\chrome.exe" set "CHROME=C:\Program Files\Google\Chrome\Application\chrome.exe"
+if exist "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" set "CHROME=C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+if exist "%LOCALAPPDATA%\Google\Chrome\Application\chrome.exe" set "CHROME=%LOCALAPPDATA%\Google\Chrome\Application\chrome.exe"
+if "%CHROME%"=="" if exist "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" set "CHROME=C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+if "%CHROME%"=="" if exist "C:\Program Files\Microsoft\Edge\Application\msedge.exe" set "CHROME=C:\Program Files\Microsoft\Edge\Application\msedge.exe"
+if "%CHROME%"=="" (start "" "%USERPROFILE%\.gbt\launcher.html") else (start "" "%CHROME%" --new-window "%USERPROFILE%\.gbt\launcher.html")
